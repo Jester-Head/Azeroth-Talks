c0e73519aba70dc00c6edcd7ef9f61b866ec139a
SB <19213803+Jester-Head@users.noreply.github.com>
2023-12-16 03:28:11 -0500

    Update forum URL in spider callback

--- classic/classic/spiders/classic_forums.py
+++ classic/classic/spiders/classic_forums.py
@@ -13,7 +13,7 @@ class SodForumsSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(allow=('^https://us.forums.blizzard.com/en/wow/t/'),
-             deny=('^https://worldofwarcraft.com/en-us/character/', '^https://us.forums.blizzard.com/en/wow/u/', 'https://us.forums.blizzard.com/en/wow/g/blizzard-tracker','https://us.forums.blizzard.com/en/wow/t/get-started-finding-people-here/215459')), callback='parse_thread'),
+             deny=('^https://worldofwarcraft.com/en-us/character/', '^https://us.forums.blizzard.com/en/wow/u/', 'https://us.forums.blizzard.com/en/wow/g/blizzard-tracker','https://us.forums.blizzard.com/en/wow/t/get-started-finding-people-here/215459','https://us.forums.blizzard.com/en/wow/c/wow-classic/wow-classic-lfg-lfm/201')), callback='parse_thread'),
         Rule(LinkExtractor(restrict_css="span > b", deny=('#suggested-topics',
              'div.topic-map', 'div.post-links-container', 'h3', 'div > aside')), follow=True)
     )
@@ -22,15 +22,16 @@ class SodForumsSpider(CrawlSpider):
         comments = response.xpath('//div[@class="post"]')
 
         title = response.xpath('//h1/a/text()').extract_first()
+        forum = response.xpath('//*[@id="topic-title"]/div/span[2]/a/span[2]/span/text()').extract_first()
 
         for comment in comments:
             item = WowClassicItem()
             item['topic'] = title
-            item['forum'] = response.xpath(
-                '//*[@id="topic-title"]/div/span[2]/a/span[2]/span/text()').extract_first()
+            item['forum'] = forum  
             item['comment'] = {
-                'text': comment.xpath('.//p/text()').extract(),
-                'likes' :comments.xpath('//span[contains(text(),\'Likes\')]/text()').extract_first(),
-                'date': comments.xpath('//span[@class="crawler-post-infos"]/time[@itemprop="datePublished"]/@datetime').extract_first()
+                'text':' '.join(comment.xpath('.//p/text()').extract()),
+                'likes':comments.xpath('//span[contains(text(),\'Likes\')]/text()').extract_first(),
+                'date':comments.xpath('//span[@class="crawler-post-infos"]/time[@itemprop="datePublished"]/@datetime').extract_first()
             }
             yield item
+
